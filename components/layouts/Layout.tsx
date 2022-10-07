e914fa3c438004dce24c62514c069b5cfa5e358e
Mariano <macacozzz@hotmail.com>
2022-10-06 22:35:58 -0300

    me equivoque de extension de la imagen para el meta property

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -23,7 +23,7 @@ export const Layout: FC<Props> = ({ children, title }) => {
 
                 <meta property="og:title" content={`Informacion sobre ${title}`} />
                 <meta property="og:description" content={`Esta es la pagina sobre ${ title }`} />
-                <meta property="og:image" content={`${ origin }/img/banner.jpg`} />
+                <meta property="og:image" content={`${ origin }/img/banner.png`} />
             </Head>
 
             <Navbar />
